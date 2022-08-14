a2c6a60763e0b0fab924b1e9795fa9603911506d
SAK74 <sergijks@gmail.com>
2022-08-14 20:48:04 +0200

    add env.PORT

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,13 @@
-const app = require('express')();
-const cors = require('cors');
+const app = require("express")();
+const cors = require("cors");
 
-const PORT = 4000; 
+const PORT = process.env.PORT || 4000;
 const corsOptions = {
-    origin: true //["http://localhost:3000", "http://192.168.0.56"]
+  origin: true, //["http://localhost:3000", "http://192.168.0.56"]
 };
 app.use(cors(corsOptions));
 
-app.all('/', (req, res)=>{
-    res.status(200)
-    .json({test: "something"});
+app.all("/", (req, res) => {
+  res.status(200).json({ test: "something" });
 });
 app.listen(PORT, () => console.log(`App listening in port ${PORT}`));
