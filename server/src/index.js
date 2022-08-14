fc3da3bf3d9a84f71045a6f93ab367ba3d61a7eb
SAK74 <sergijks@gmail.com>
2022-08-14 21:36:45 +0200

    add public

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const cors = require("cors");
 
 const PORT = process.env.PORT || 4000;
@@ -6,7 +7,7 @@ const corsOptions = {
   origin: true, //["http://localhost:3000", "http://192.168.0.56"]
 };
 app.use(cors(corsOptions));
-
+app.use(express.static("public"));
 app.all("/", (req, res) => {
   try {
     res.status(200).json({ test: "something" });
