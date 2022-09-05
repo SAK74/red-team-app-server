85a02d7632a302aa5062d0c17766214a09b635a0
SAK74 <sergijks@gmail.com>
2022-09-05 21:48:47 +0200

    initial

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const app = require("express")();
 const cors = require("cors");
 
 const PORT = process.env.PORT || 4000;
-console.log(process.env);
 const corsOptions = {
   origin: true, //["http://localhost:3000", "http://192.168.0.56"]
 };
